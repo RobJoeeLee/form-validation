0acbf496f5ab52870e1d2c14d823dbdae5b63130
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2024-06-14 16:38:59 -0400

    creating/selecting my inputs in js

---
+++ script.js
@@ -0,0 +1,5 @@
+const name = document.getElementById("name");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("name");
+
